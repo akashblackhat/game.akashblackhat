01ea1ccddf51dd4f8b91ff3706c48a2a71d36dbf
akashBlackHat <88341460+akashblackhat@users.noreply.github.com>
2022-08-15 23:06:58 +0530

    Add files via upload

---
+++ Screen.py
@@ -0,0 +1,6 @@
+import pygame
+pygame.init()
+dis=pygame.display.set_mode((400,300))
+pygame.display.update()
+pygame.quit()
+quit()
---
+++ while loop.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+dis = pygame.display.set_mode((400, 300))
+pygame.display.update()
+pygame.display.set_caption('Developer By.akashblackhat(-_-)akashram8090as@gmail.com.=python developer')
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)  # helo my name is akash ima python developer.&. hacking tool mack termux kali linux
+
+pygame.quit()
+quit()
